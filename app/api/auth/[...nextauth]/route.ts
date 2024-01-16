c1a5368672035dac726f2e7d49c0794966ae4203
ARoble <abdulladif0042@gmail.com>
2024-01-16 17:37:50 +0300

    fix: exported auth options

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -48,4 +48,4 @@ const authOptions: AuthOptions = {
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST, authOptions };
