a184a9a14b807b4aa4b2343c2e3f63c950bc3cd4
ARoble <abdulladif0042@gmail.com>
2024-01-20 14:23:56 +0300

    fix: env update

--- app/Utilities/fetch/index.ts
+++ app/Utilities/fetch/index.ts
@@ -1,32 +1,38 @@
 import axios from "axios";
 
 export async function uploadSetup(formData: FormData) {
-  const res = await axios.post("http://localhost:3000/api/setup", formData, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-    onUploadProgress: (event) => {
-      return Math.round(100 * event.loaded) / event.total!;
-    },
-  });
+  const res = await axios.post(
+    `${process.env.NEXT_PUBLIC_BASE_URL}api/setup`,
+    formData,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      onUploadProgress: (event) => {
+        return Math.round(100 * event.loaded) / event.total!;
+      },
+    }
+  );
 
   return res.data.setup;
 }
 
 export async function fetchSetups() {
-  const res = await axios.get("http://localhost:3000/api/setup");
+  const res = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}api/setup`);
 
   return res.data.setups;
 }
 
 export async function deleteSetup(id: string) {
-  await axios.delete(`http://localhost:3000/api/setup/${id}`);
+  await axios.delete(`${process.env.NEXT_PUBLIC_BASE_URL}api/setup/${id}`);
 
   return;
 }
 
 export async function likeSetup(id: string) {
-  const res = await axios.post(`http://localhost:3000/api/setup/like/${id}`);
+  const res = await axios.post(
+    `${process.env.NEXT_PUBLIC_BASE_URL}api/setup/like/${id}`
+  );
 
   return res.data.like ? res.data.like : undefined;
 }
